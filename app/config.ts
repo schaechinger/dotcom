e355661f7a906d8d8c1f1e9259f7beaec74e20d2
Manuel Schächinger <manuel@schaechinger.com>
2024-08-26 08:52:06 +0200

    fix: renamed env variables for browser use

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,5 @@
 export const PROD_HOST = 'https://www.schaechinger.com';
-export const WEB_HOST = process.env.WEB_HOST || PROD_HOST;
+export const WEB_HOST = process.env.NEXT_PUBLIC_WEB_HOST || PROD_HOST;
 export const IMAGE_HOST = 'https://images.schaechinger.com';
 export const STATIC_HOST = 'https://static.schaechinger.com';
 
