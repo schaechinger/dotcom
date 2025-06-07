e74f209d01c572805c663deb1aea8a7e6c8eafe4
Manuel Schächinger <manuel@schaechinger.com>
2025-06-07 23:57:17 +0200

    feat: updated now page, fixed header padding

--- app/components/organisms/NowList.tsx
+++ app/components/organisms/NowList.tsx
@@ -2,13 +2,21 @@ import { getLocale } from 'next-intl/server';
 
 import { loadNow } from '@lib/contentful';
 import { type LocaleCode } from '@lib/router';
-import BulletList from '../atoms/BulletList';
 
 const NowList = async () => {
   const locale = await getLocale() as LocaleCode;
   const activities = await loadNow(locale) || [];
 
-  return <BulletList bullets={activities} />;
+  return (
+    <div className="grid sm:grid-cols-2 gap-4">
+      {activities.map((activity, i) => (
+        <div key={i} className="mb-4">
+          <h2 className="text-h2">{activity.category}</h2>
+          <p className="mt-2">{activity.description}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default NowList;
